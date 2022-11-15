d5492c8b493f3aa06128cf2298aac198f474c936
seungmi yang <seungmiyang427@gmail.com>
2022-11-15 15:20:04 -0500

    Update Damage Code
    organized + added comments explaining code

--- UNITY - Angry Bird Clone Project/Assets/Scripts/Damage.cs
+++ UNITY - Angry Bird Clone Project/Assets/Scripts/Damage.cs
@@ -4,34 +4,34 @@ using UnityEngine;
 
 public class Damage : MonoBehaviour {
 
-	public int HP = 3;
-	public float damageImpactSpeed; //speed threshold for registering hit
+	public int HP = 3; //health points
+	public float damageImpactSpeed; //speed/force threshold for determining whether a hit is registered or not. birds will need this in their code.
 	public GameObject gameObj;
-	//public Sprite damagedSprite;
+	//public Sprite damagedSprite; ***will add when sprites are ready!***
 
 	private int currentHP;
 	private float damageImpactSpeed2;
 	private Collider2D objCollider2D;
 	private Rigidbody2D objRigidbody2D;
-	//private SpriteRenderer spriteRenderer;
+	//private SpriteRenderer spriteRenderer; ***will add when sprites are ready!***
 
 
 	void Start()
 	{
-		//spriteRenderer = GetComponent <SpriteRenderer> ();
-		currentHP = HP; //because different materials have different hitpoints
-		damageImpactSpeed2 = damageImpactSpeed * damageImpactSpeed;
+		//spriteRenderer = GetComponent <SpriteRenderer> (); ***will add when sprites are ready!***
+		currentHP = HP; //this is needed because different materials have different health points
+		damageImpactSpeed2 = damageImpactSpeed * damageImpactSpeed; //speed/force threshold
 		objCollider2D = GetComponent<Collider2D>();
 		objRigidbody2D = GetComponent<Rigidbody2D>();
 	}
 	void OnCollisionEnter2D(Collision2D collision)
 	{
-		if (collision.relativeVelocity.sqrMagnitude < damageImpactSpeed2)
+		if (collision.relativeVelocity.sqrMagnitude < damageImpactSpeed2) //if velocity of collision is LESS THAN speed/force threshold, don't register the hit
 		{
 			return;
 		}
-		//spriteRenderer.sprite = damagedSprite;
-		currentHP --;
+		//spriteRenderer.sprite = damagedSprite; ***will add when sprites are ready!***
+		currentHP--;
 		Debug.Log("Hit");
 
 		if (currentHP <= 0)
@@ -41,7 +41,7 @@ public class Damage : MonoBehaviour {
 	}
 	void Death()
 	{
-		//spriteRenderer.enabled = false;
+		//spriteRenderer.enabled = false; ***will add when sprites are ready!***
 		objCollider2D.enabled = false;
 		objRigidbody2D.isKinematic = true;
 		Debug.Log("Dead");
