6bdaa9d0708e249d25e0c3b92d9c91df8a3386ac
Firemanwolf <57378965+Firemanwolf@users.noreply.github.com>
2022-11-16 09:15:18 -0500

    update of launchig

--- UNITY - Angry Bird Clone Project/Assets/Scripts/Bird.cs
+++ UNITY - Angry Bird Clone Project/Assets/Scripts/Bird.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class Bird : MonoBehaviour
 {
     private bool isClick = false;
+    public Transform rightPos;
+    public float maxDis = 1.2f;
+    private SpringJoint2D sp;
+
+    private void Awake()
+    {
+        sp = GetComponent<SpringJoint2D>();
+    }
 
     private void OnMouseDown()
     {
@@ -14,6 +22,7 @@ public class Bird : MonoBehaviour
     private void OnMouseUp()
     {
         isClick = false;
+        sp.enabled = false;
     }
 
     private void Update()
@@ -23,7 +32,14 @@ public class Bird : MonoBehaviour
             //keep tracking the position of the mouse while pressing
             transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             transform.position += new Vector3(0, 0, -Camera.main.transform.position.z);
-        }
-          
+            if (Vector3.Distance(transform.position, rightPos.position) > maxDis)
+            {
+                //Restraining dragging distance
+                Vector3 pos = (transform.position - rightPos.position).normalized;//Find the direction of dragging 
+                pos *= maxDis;//maximum length vector;
+                transform.position = pos + rightPos.position;
+            }
+        } 
+
     }
 }
